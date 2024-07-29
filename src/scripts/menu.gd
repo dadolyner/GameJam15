1a88046dc6e0249cf4319365bf7006dc454d590e
dkonc <davidkonc@hotmail.com>
2024-07-29 19:31:44 +0200

    video + menu

--- src/scripts/menu.gd
+++ src/scripts/menu.gd
@@ -1,15 +1,29 @@
 extends Control
 
 @onready var start_button: Button = $VBoxContainer/StartButton
+@onready var video_stream_player = $VideoStreamPlayer
+@onready var margin_container = $MarginContainer
+@onready var v_box_container = $VBoxContainer
+@onready var skip_button = $SkipButton
 
 func _ready() -> void:
 	start_button.grab_focus()
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file(Globals.menu.levels)
+	margin_container.visible = false
+	v_box_container.visible = false
+	skip_button.visible = true
+	video_stream_player.play()
 	
 func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_file(Globals.menu.options)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_video_stream_player_finished():
+	get_tree().change_scene_to_file(Globals.menu.levels)
+
+func _on_skip_button_pressed():
+	skip_button.visible = false
+	get_tree().change_scene_to_file(Globals.menu.levels)
